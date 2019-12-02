bc0f333be07a1c9f6d7bca8d04192201a03dcc87
Ron Cooke <ron@astonspark.com>
2019-12-01 23:24:13 -0500

    Day 1 - Part 2

--- day01/fuel_calc.exs
+++ day01/fuel_calc.exs
@@ -1,4 +1,4 @@
-defmodule FuleCalc do
+defmodule FuelCalc do
   def call(input_path) do
     File.stream!(input_path)
     |> Enum.reduce(0, fn line, acc ->
@@ -6,13 +6,21 @@ defmodule FuleCalc do
                |> String.trim
                |> String.to_integer
 
-      acc + calculate(module)
+      acc + recalculate(module)
     end)
   end
 
-  def calculate(module) do
+  defp calculate(module) do
     div(module, 3) - 2
+  end
+
+  defp recalculate(module) do
+    result = calculate(module)
+    case result < 1 do
+      true -> 0
+      false -> result + recalculate(result)
+    end
   end
 end
 
-IO.puts FuleCalc.call("input.txt")
+IO.puts FuelCalc.call("input.txt")
