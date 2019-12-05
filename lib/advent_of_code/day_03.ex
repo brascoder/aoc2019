a0831148e925efc8856bfea68f7db942f5dc97be
Ron Cooke <ron@astonspark.com>
2019-12-05 07:20:05 -0500

    WIP - Day 02 - Part 2

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -6,6 +6,12 @@ defmodule AdventOfCode.Day03 do
     |> find_shortest_distance
   end
 
+  def part2(input_stream) do
+    input_stream
+    |> Stream.map(&line_points/1)
+    |> find_intersection_indexes
+  end
+
   defp line_points(line) do
     line
     |> String.trim
@@ -38,6 +44,25 @@ defmodule AdventOfCode.Day03 do
     lines
     |> Stream.map(&MapSet.new/1)
     |> Enum.reduce(&MapSet.intersection/2)
+  end
+
+  defp find_intersection_indexes(lines) do
+    intersections = find_intersections(lines)
+                    |> IO.inspect
+
+    Enum.into(lines, []) |> IO.inspect
+    lines
+               |> Enum.map(fn line ->
+                 line
+                 |> IO.inspect
+                 |> Enum.filter(fn point -> Enum.member?(intersections, point) end)
+                 |> Enum.map(fn point -> {point, Enum.find_index(line, point) + 1} end)
+                 |> Enum.min_by(fn {_, i} -> i end)
+               end)
+               |> IO.inspect
+
+    # for({p1, i1} <- l1, {p2, i2} <- l2, p1 == p2, do: {p1, i1 + i2})
+    # |> Enum.min_by(fn {_, i} -> i end)
   end
 
   defp find_shortest_distance(points) do
